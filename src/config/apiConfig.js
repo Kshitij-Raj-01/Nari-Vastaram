7e69110715ce753efb7aa537012a24f5662e6fa9
Kshitij-Raj-01 <kshwebsites@gmail.com>
2025-07-19 21:46:59 +0530

    Update

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://nari-vastaram-backend.onrender.com"
+export const API_BASE_URL = "https://narivastaram.com"
 
 const jwt = sessionStorage.getItem("jwt")
 
