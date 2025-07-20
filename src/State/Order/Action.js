b5d3718ca1ab3cae3a533e5fc037ebfea48c57e8
Kshitij Raj <155631442+Kshitij-Raj-01@users.noreply.github.com>
2025-07-20 15:44:44 +0530

    Update Action.js

--- src/State/Order/Action.js
+++ src/State/Order/Action.js
@@ -10,25 +10,30 @@ import {
 
 export const createOrder = (reqData) => async (dispatch) => {
   dispatch({ type: CREATE_ORDER_REQUEST });
+
   try {
     const { data } = await api.post("/api/orders", reqData.address);
+    console.log("actual data : ", data);
     dispatch({ type: CREATE_ORDER_SUCCESS, payload: data });
-    if (reqData.navigate && data._id) {
+
+    if (data._id) {
       reqData.navigate({ search: `step=3&order_id=${data._id}` });
     }
-    return { payload: data }; // so caller knows order ID
   } catch (error) {
+    console.log("catch error : ", error);
     dispatch({ type: CREATE_ORDER_FAILURE, payload: error.message });
-    return { error: error.message };
   }
 };
 
 export const getOrderById = (orderId) => async (dispatch) => {
   dispatch({ type: GET_ORDER_BY_ID_REQUEST });
+
   try {
     const { data } = await api.get(`/api/orders/${orderId}`);
+    console.log("order by id ", data);
     dispatch({ type: GET_ORDER_BY_ID_SUCCESS, payload: data });
   } catch (error) {
+    console.log("catch error : ", error);
     dispatch({ type: GET_ORDER_BY_ID_FAILURE, payload: error.message });
   }
 };
