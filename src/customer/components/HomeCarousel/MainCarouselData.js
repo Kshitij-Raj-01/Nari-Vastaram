a981971a9be279d9e0eecefa95e6560ff4717d10
Kshitij Raj <155631442+Kshitij-Raj-01@users.noreply.github.com>
2025-07-30 18:37:06 +0530

    Update MainCarouselData.js

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -5,4 +5,10 @@ export const MainCarouselData = [
     {
         image : "https://res.cloudinary.com/dg2rjn4qj/image/upload/v1752930287/Bold_Sale_Brand_Website_Homepage_Banner_1_glze1m.png"
     },
+    {
+        image : "https://res.cloudinary.com/dg2rjn4qj/image/upload/v1753880759/Cream_and_Gold_Raksha_Bandhan_Desktop_Wallpaper_sbxh9v.png"
+    },
+    {
+        image : "https://res.cloudinary.com/dg2rjn4qj/image/upload/v1753880761/Red_and_Gold_Traditional_Indian_Raksha_Bandhan_Greeting_Banner_xrwmys.png"
+    },
 ]
