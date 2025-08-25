ec43be74900527d248a397cbbbf4b38386bae53f
Kshitij Raj <155631442+Kshitij-Raj-01@users.noreply.github.com>
2025-08-25 20:53:23 +0530

    Update MainCarousel.jsx

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -4,18 +4,44 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import { MainCarouselData } from './MainCarouselData';
 
 const MainCarousel = () => {
-    
-    const items = MainCarouselData.map((item) => <img className='cursor-pointer' role='presentation' src={item.image} alt="" />)
 
-    return(
+  const items = MainCarouselData.map((item) => {
+    // Check if the item's image URL is a video
+    const isVideo = item.image.endsWith('.mp4');
+
+    if (isVideo) {
+      return (
+        <video
+          className='cursor-pointer'
+          autoPlay
+          loop
+          muted
+          src={item.image}
+          alt=""
+        />
+      );
+    } else {
+      return (
+        <img
+          className='cursor-pointer'
+          role='presentation'
+          src={item.image}
+          alt=""
+        />
+      );
+    }
+  });
+
+  return (
     <AliceCarousel
-        items={items}
-        disableButtonsControls
-        disableDotsControls
-        autoPlay
-        autoPlayInterval={2500}
-        infinite
+      items={items}
+      disableButtonsControls
+      disableDotsControls
+      autoPlay
+      autoPlayInterval={2500}
+      infinite
     />
-)};
+  );
+};
 
 export default MainCarousel;
