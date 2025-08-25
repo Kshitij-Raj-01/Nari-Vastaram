d05477110d5e650a065e80cb317115bb22c6ab11
Kshitij Raj <155631442+Kshitij-Raj-01@users.noreply.github.com>
2025-08-25 20:52:57 +0530

    Update MainCarouselData.js

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,8 +1,5 @@
 export const MainCarouselData = [
     {
-        image : "https://res.cloudinary.com/dg2rjn4qj/image/upload/v1753880759/Cream_and_Gold_Raksha_Bandhan_Desktop_Wallpaper_sbxh9v.png"
-    },
-    {
-        image : "https://res.cloudinary.com/dg2rjn4qj/image/upload/v1753880761/Red_and_Gold_Traditional_Indian_Raksha_Bandhan_Greeting_Banner_xrwmys.png"
-    },
+        image : "/assets/Ganesh Chaturthi Banner.mp4"
+    }
 ]
